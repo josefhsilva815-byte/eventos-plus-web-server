633c9c25522bcda6f1ae3c60a1302fe2ea43deb8
josefhsilva815-byte <josefhsilva815@gmail.com>
2025-08-20 15:56:11 -0300

    Integração do tailwindcss para estilização do projeto

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const db = require("./src/database/config.js");
+const path = require("node:path");
 const router = require("./src/routes/index.js");
 
 const app = express();
 app.use(express.json());
 
-app.use("/", router)
+app.use(express.static(path.join(__dirname, "src/public")));
+
+app.use("/", router);
 
 app.listen(3000, () => {
     console.log("Servidor rodando na porta http://localhost:3000");
-})
+});
