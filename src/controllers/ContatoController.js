7140a2497baa3f25bc9fdf528b0d182cb6e0d1d3
josefhsilva815-byte <josefhsilva815@gmail.com>
2025-08-25 14:12:29 -0300

    Implementa página de agradecimento

--- src/controllers/ContatoController.js
+++ src/controllers/ContatoController.js
@@ -11,11 +11,7 @@ module.exports = {
         try {
             (await db("contatos").insert({nome, email, whatsapp, mensagem}));
             console.log("📩 Novo contato salvo no banco de dados");
-            res.send(`
-                    <h1>Obrigado pelo seu contato, ${nome}</h1>
-                    <p>Sua mensagem foi registrada e em breve entraremos em contato.</p>
-                    <a href="http://localhost:3000">Voltar para Home</a>
-                `)
+            res.sendFile(path.join(__dirname, "../views/agradecimento.html"))
         } catch (error) {
             console.error("Erro ao salvar seu contato:", error);
             res.status(500).send("Erro ao enviar a mensagem. Tente novamente mais tarde.")
